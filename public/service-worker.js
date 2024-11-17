40ace3f321668742b5016e2748843af778c9bbda
joseGomez <jose_armando_za@hotmail.com>
2024-11-16 20:48:20 -0600

    first merge vue pwa

---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+// Al instalar el Service Worker, se almacenan los archivos en caché
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open('reloj-pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon/tiempo.png',
+        '/icon/reloj.png',
+        '/app.js',
+        '/styles.css',
+      ]);
+    })
+  );
+});
+
+// Al activar el Service Worker, eliminamos cualquier caché anterior
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated.');
+  const cacheWhitelist = ['reloj-pwa-cache']; 
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/hora')) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then((networkResponse) => {
+          if (networkResponse.ok) {
+            caches.open('reloj-pwa-cache').then((cache) => {
+              cache.put(event.request, networkResponse.clone());
+            });
+          }
+          return networkResponse;
+        });
+      }).catch(() => {
+        return caches.match('/index.html');
+      })
+    );
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1>Hora Actual</h1>
+      <div class="clock-container">
+        <!-- Icono izquierdo -->
+        <img src="/icon/tiempo.png" alt="Icono de tiempo" class="icon-left" />
+        <!-- Hora actual -->
+        <div class="clock">
+          <span>{{ time }}</span>
+        </div>
+        <!-- Icono derecho -->
+        <img src="/icon/reloj.png" alt="Icono de reloj" class="icon-right" />
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "@/assets/styles.css"; // Ruta al archivo CSS
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  data() {
+    return {
+      time: '',
+    };
+  },
+  methods: {
+    updateClock() {
+      const now = new Date();
+      this.time = now.toLocaleTimeString(); // Formato de hora local
+    },
+  },
+  mounted() {
+    // Registra el Service Worker
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registrado con éxito:', registration);
+        })
+        .catch((error) => {
+          console.log('Error al registrar el Service Worker:', error);
+        });
+    }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    this.updateClock(); // Establece la hora al cargar
+    setInterval(this.updateClock, 1000); // Actualiza la hora cada segundo
+  },
+};
+</script>
